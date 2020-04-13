0be929db518b720a09e998388736c2f347f650ff
valeriekamen <valerie@lyft.com>
2020-04-12 20:57:58 -0700

    farenheit

--- weather.py
+++ weather.py
@@ -7,6 +7,7 @@
 import os
 import json
 import time
+import re
 import urllib
 import logging
 from sys import exit
@@ -66,16 +67,20 @@ def get_weather(coords):
         res = requests.get(url)
         if res.status_code == 200:
             soup = BeautifulSoup(res.content, "lxml")
-            weather['feelslike'] = soup.find("span", "feels-like-text").text
-            weather['high'] = soup.find("span", "high-temp-text").text
-            weather['low'] = soup.find("span", "low-temp-text").text
             weather['summary'] = soup.find("span", "summary").text
+            weather['feelslike'] = make_farenheit(soup.find("span", "feels-like-text").text)
+            weather['high'] = make_farenheit(soup.find("span", "high-temp-text").text)
+            weather['low'] = make_farenheit(soup.find("span", "low-temp-text").text)
     except requests.exceptions.RequestException as e:
         logging.error("Could not get weather data from DarkSky: {}".format(e))
         pass
 
     return weather
 
+
+def make_farenheit(temp):
+    t = re.sub("[^0-9]", "", temp)
+    return str(int((9 * int(t))/5 + 32))
 
 def scroll_message(output):
     scrollphat.write_string(output)
